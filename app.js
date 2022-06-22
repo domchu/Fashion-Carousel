1bc3f04441727b34ff790ea093d12b7901c8f97d
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-06-22 14:29:06 +0100

    added the button functionalities

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
 let sliders = document.querySelectorAll(".slider");
 let leftBtn = document.querySelector(".left");
 let rightBtn = document.querySelector(".right");
+
+let currentPosition = 0;
+
+// the left button
+leftBtn.addEventListener("click", () => {
+  if (currentPosition == 0) {
+    currentPosition = 3;
+    // console.log(currentPosition);
+  }
+  currentPosition -= 1;
+
+  for (let index = 0; index < sliders.length; index++) {
+    sliders[index].style.transform = `translate(-${currentPosition}00%)`;
+  }
+});
+
+// the right button
+rightBtn.addEventListener("click", () => {
+  if (currentPosition > sliders.length - 1) {
+    rightBtn.style.opacity = "0.4";
+    rightBtn.style.cursor = "default";
+  } else {
+    rightBtn.style.opacity = "1";
+    rightBtn.style.cursor = "pointer";
+    currentPosition += 1;
+    if (currentPosition > sliders.length - 3) {
+      currentPosition = 0;
+    }
+    for (let index = 0; index < sliders.length; index++) {
+      sliders[index].style.transform = `translate(-${currentPosition}00%)`;
+    }
+  }
+});
